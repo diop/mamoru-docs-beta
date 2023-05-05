b898b47a5412d1f13f2848fd06088fdf7bf7195d
Fodé Diop <diop@Fodes-MacBook-Pro.local>
2023-05-05 06:50:55 -0500

    first commit

---
+++ theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+    logo: <span>Mamoru.ai</span>,
+    footer: {
+        text: 'Mamoru.ai 2023',
+    },
+    project: {
+        link: 'https://github.com/mamoru-foundation'
+    },
+    chat: {
+        link: 'https://discord.gg'
+    }
+}
